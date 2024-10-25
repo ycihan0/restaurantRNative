54708d97a5bd1463b776092414357c77bc26593e
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-10-25 21:57:00 +0300

    react navigate

--- App.js
+++ App.js
@@ -4,17 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! denem</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ 
 });
