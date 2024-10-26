bfd2f963818f148348cc4e36dbf4f97ae1c42eb1
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-10-26 12:39:20 +0300

    Search CSS

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,12 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import React from "react";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 export default function SearchBar() {
   return (
-    <View>
-      <Text>SearchBar</Text>
+    <View style={styles.backgroundStyle}>
+      <AntDesign
+        style={styles.iconStyle}
+        name="search1"
+        size={24}
+        color="black"
+      />
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Ara"
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: "lightgray",
+    flexDirection: "row",
+    margin: 10,
+    height: 50,
+    alignItems: "center",
+    borderRadius: 20,
+  },
+  iconStyle: { marginHorizontal: 15 },
+  inputStyle: { flex: 1, fontSize: 18 },
+});
