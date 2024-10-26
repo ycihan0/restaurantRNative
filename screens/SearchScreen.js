c193c1f4356686ba67fde219e8b09ee6ce3efa75
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-10-26 19:40:14 +0300

    restaurant filtre

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -2,13 +2,35 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 export default function SearchScreen() {
   const [searchApi, result] = useResults();
-  console.log(result)
+  
+  const filterResultsByPrice = (price) => {
+    return result.filter((result) => {
+      return result.price === price;
+    });
+  };
   return (
     <View>
       <SearchBar />
+      <ResultsList
+        title="Ucuz Restorantlar"
+        results={filterResultsByPrice("₺")}
+      />
+      <ResultsList
+        title="Uygun Restorantlar"
+        results={filterResultsByPrice("₺₺")}
+      />
+      <ResultsList
+        title="Pahalı Restorantlar"
+        results={filterResultsByPrice("₺₺₺")}
+      />
+      <ResultsList
+        title="Lüx Restorantlar"
+        results={filterResultsByPrice("₺₺₺₺")}
+      />
     </View>
   );
 }
