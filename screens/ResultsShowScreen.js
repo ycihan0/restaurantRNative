cc77c7be653a80fe9d7599d8719e9eeb036a9e1d
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-10-29 17:49:18 +0300

    resultShowScreen Css

--- screens/ResultsShowScreen.js
+++ screens/ResultsShowScreen.js
@@ -1,12 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, View, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import yelp from "../api/yelp";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
-export default function ResultsShowScreen() {
+export default function ResultsShowScreen({ route }) {
+  const [result, setResult] = useState(null);
+  const id = route.params.id;
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    console.log(response.data);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  if (!result) {
+    return null;
+  }
+
   return (
     <View>
-      <Text>ResultsShowScreen</Text>
+      <Text style={styles.title}>{result.name}</Text>
+      <Text style={styles.phone}>{result.phone}</Text>
+      <View style={styles.icon}>
+        {result.is_closed ? (
+          <AntDesign name="closecircle" size={30} color="black" />
+        ) : (
+          <MaterialIcons name="delivery-dining" size={30} color="black" />
+        )}
+      </View>
+      <FlatList
+        data={result.photos}
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />;
+        }}
+      />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  image: {
+    height: 180,
+    margin: 10,
+    borderRadius: 20,
+  },
+  title: {
+    alignSelf: "center",
+    fontSize: 25,
+    marginVertical: 10,
+  },
+  phone: {
+    alignSelf: "center",
+    fontSize: 20,
+  },
+  icon: {
+    alignSelf: "center",
+  },
+});
